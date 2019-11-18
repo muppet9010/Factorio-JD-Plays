8a6f3af85c8af260d717ebb60720adb74c409115
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2019-11-18 12:14:32 +0000

    fix rocket silo spawn distance direction

--- modes/december-2019/scripts/rocket-silo.lua
+++ modes/december-2019/scripts/rocket-silo.lua
@@ -4,7 +4,7 @@ local Utils = require("utility/utils")
 --local Logging = require("utility/logging")
 
 local yChunkOffsetWeighting = 0.95
-local yChunkStarting = 5000
+local yChunkStarting = -5000
 
 RocketSilo.CreateGlobals = function()
     global.RocketSilo = global.RocketSilo or {}
