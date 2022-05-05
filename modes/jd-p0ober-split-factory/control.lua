dd9179d7f3245ee7361326a6fbc0cf527aca01ae
Muppet9010 <mike.hollett@gmail.com>
2022-05-05 12:19:27 +0100

    Removed old unused Utility file
    Should have been removed as part of Utils update previously.
    Updated old JD & Poober mode that used the old Interfaces library.

--- modes/jd-p0ober-split-factory/control.lua
+++ modes/jd-p0ober-split-factory/control.lua
@@ -39,3 +39,13 @@ script.on_configuration_changed(OnStartup)
 --script.on_event(defines.events.on_runtime_mod_setting_changed, OnSettingChanged)
 script.on_load(OnLoad)
 EventScheduler.RegisterScheduler()
+
+-- Mod wide function interface table creation. Means EmmyLua can support it and saves on UPS cost of old Interface function middelayer.
+---@class InternalInterfaces
+MOD.Interfaces = MOD.Interfaces or {} ---@type table<string, function>
+--[[
+    Populate and use from within module's OnLoad() functions with simple table reference structures, i.e:
+        MOD.Interfaces.Tunnel = MOD.Interfaces.Tunnel or {}
+        MOD.Interfaces.Tunnel.CompleteTunnel = Tunnel.CompleteTunnel
+--]]
+--
